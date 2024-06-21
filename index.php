b865b11baf2c0c2bc0c8b536757a91f480a1ccd9
alexbberlindienstplan <alex.b.berlin.dienstplan@gmail.com>
2024-06-21 12:31:35 +0200

    code cleaning

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!doctype html>
-<?php require_once '.assets/ini.php';?>
+<?php require_once '.assets/ini.php'; ?>
 <html lang="de">
+
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1" name="viewport">
@@ -10,6 +11,7 @@
     <link href="https://fonts.cdnfonts.com/css/architects-daughter" rel="stylesheet">
     <link href=".assets/localstyle.css" rel="stylesheet">
 </head>
+
 <body class="bg-dark">
     <div class="container-fluid">
         <div class="d-flex justify-content-center align-items-center bg-secondary rounded-4 blue-border">
@@ -22,41 +24,32 @@
                     <?php echo outputInidata(); ?>
                 </ul>
 
-
                 <ul class="list-group bg-dark">
-
-                    <li class="list-group-item col-12 text-center text-yellow border-0 py-0">
-                        <h4>Hostliste
-                            (<?php echo count(listHosts()); ?>)
-
-                        </h4>
-                    </li>
+                    <li class="list-group-item col-12 text-center text-yellow border-0 py-0"><h4>Hostliste (<?php echo count(listHosts()); ?>)</h4></li>
                     <li class="list-group-item">
                         <ul class="list-group list-group-horizontal d-flex py-0 bg-transparent">
                             <?php
-							$projectItems = listHosts();
-							foreach ($projectItems as $projectName) {
-								echo '<li class="list-group-item flex-shrink-1 text-start text-yellow border-0 py-0">
-					<a href="' . $projectName . '" target="_blank" class="text-decoration-none text-red">
-					<i class="ps-1 bi bi-door-open"></i>' . $projectName . '</a></li>';
-							} ?>
-
+                                $projectItems = listHosts();
+                                foreach ($projectItems as $projectName)
+                                    {
+                                        echo '
+                                        <li class="list-group-item flex-shrink-1 text-start text-yellow border-0 py-0">
+                                            <a href="' . $projectName . '" target="_blank" class="text-decoration-none text-red">
+                                            <i class="ps-1 bi bi-door-open"></i>' . $projectName . '</a>
+                                        </li>';
+                                    }
+                            ?>
                         </ul>
                     </li>
                 </ul>
 
-
                 <ul class="list-group bg-dark">
-                    <li class="list-group-item col-12 text-center text-yellow border-0 py-0">
-                        <h4>Infotools</h4>
-                    </li>
+                    <li class="list-group-item col-12 text-center text-yellow border-0 py-0"><h4>Infotools</h4></li>
                     <li class="list-group-item">
                         <ul class="list-group list-group-horizontal d-flex py-0 bg-transparent">
                             <li class="list-group-item col-3 text-start text-blue border-0 py-0">Laragon Manual</li>
                             <li class="list-group-item flex-shrink-1 text-start text-yellow border-0 py-0">
-                                <a href="https://laragon.org/docs" target="_blank"
-                                    class="text-decoration-none text-red">Seite öffnen <i
-                                        class="bi bi-door-open"></i></a>
+                                <a href="https://laragon.org/docs" target="_blank" class="text-decoration-none text-red">Seite öffnen <i class="bi bi-door-open"></i></a>
                             </li>
                         </ul>
                     </li>
@@ -64,33 +57,26 @@
                         <ul class="list-group list-group-horizontal d-flex py-0 bg-transparent">
                             <li class="list-group-item col-3 text-start text-blue border-0 py-0">PHP Info</li>
                             <li class="list-group-item flex-shrink-1 text-start text-yellow border-0 py-0">
-                                <a href=".assets/php_info.php" target="_blank"
-                                    class="text-decoration-none text-red">Seite öffnen <i
-                                        class="bi bi-door-open"></i></a>
+                                <a href=".assets/php_info.php" target="_blank" class="text-decoration-none text-red">Seite öffnen <i class="bi bi-door-open"></i></a>
                             </li>
                         </ul>
                     </li>
                 </ul>
 
-
                 <ul class="list-group bg-dark">
-                    <li class="list-group-item col-12 text-center text-yellow border-0 py-0">
-                        <h4>Tools</h4>
-                    </li>
+                    <li class="list-group-item col-12 text-center text-yellow border-0 py-0"><h4>Tools</h4></li>
                     <li class="list-group-item">
                         <ul class="list-group list-group-horizontal d-flex py-0 bg-transparent">
                             <li class="list-group-item col-3 text-start text-blue border-0 py-0">PHP MyAdmin</li>
                             <li class="list-group-item flex-shrink-1 text-start text-yellow border-0 py-0">
-                                <a href="https://localhost/phpmyadmin/" target="_blank"
-                                    class="text-decoration-none text-red">Seite öffnen <i
-                                        class="bi bi-door-open"></i></a>
+                                <a href="https://localhost/phpmyadmin/" target="_blank" class="text-decoration-none text-red">Seite öffnen <i class="bi bi-door-open"></i></a>
                             </li>
                         </ul>
                     </li>
                 </ul>
+
             </div>
         </div>
     </div>
 </body>
-
 </html>
